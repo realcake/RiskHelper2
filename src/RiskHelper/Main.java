df663558bd946bef53a18d9b42ce0f5d84e56788
Andrew Hyatt <ahyattdev@icloud.com>
2016-04-12 17:31:57 -0700

    Cleaned up the project
    * Seperated the main() method and the GUI class
    * Made this an Eclipse Java project
    * Switched to SwingUtilities from EventQueue
    * Changed the visibility of the window field of RiskHelper from private
    to protected to Main can see it
    * Renamed RiskHelperMain to RiskHelper

---
+++ src/RiskHelper/Main.java
@@ -0,0 +1,38 @@
+package RiskHelper;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main {
+
+	protected static RiskHelper rh;
+
+	public static void main(String[] args) {
+
+		// Use the native Look and Feel
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			JFrame.setDefaultLookAndFeelDecorated(true);
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
+		// Create the GUI once the program has loaded
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					rh = new RiskHelper();
+					rh.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
